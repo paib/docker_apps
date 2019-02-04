1af9528e917ed8ebf60cba7e040288615571506d
Bhushan Pai <paib@avinetworks.com>
2019-02-03 21:22:01 -0800

    added game img and updated image_server file for blue/green

--- image_server/app/image_server.py
+++ image_server/app/image_server.py
@@ -1,30 +1,37 @@
 import os
 from flask import Flask, render_template
 import logging
+import urllib.request
 from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.debug = True
 
+#color = '#262626' #Black
+#color = '#4286f4' #Blue
+color = '#33cc33' #Green
+
 
 cwd = os.getcwd()
 hostname = os.getenv('HOSTNAME', '')
-fruit = os.getenv('FRUIT', 'green-apple')
+image = os.getenv('IMAGE', 'mario')
 my_ip = os.getenv('MY_IP', '0.0.0.0')
 site = os.getenv('SITE', '')
-vault_host = os.getenv('VAULT_URL', None)
+vault_host = os.getenv('VAULT_HOST', None)
 
 @app.route('/')
 def index():
-  return render_template('index.html', fruit=fruit, hostname=hostname, my_ip=my_ip) 
+
+  if vault_host:
+    f = open('static/img/%s.png' % image,'wb')
+    f.write(urllib.request.urlopen('http://%s/static/img/%s.png' % (vault_host, image)).read())
+    f.close()
+    return render_template('index.html', image=image, hostname=hostname, my_ip=my_ip, color=color) 
+  return "Image vault not specified"
 
 if __name__ == '__main__':
   # Log http requests
   logging.basicConfig(filename= cwd + '/logs/webapp.log', level=logging.DEBUG)
 
-  if vault_host:
-    f = open('static/img/%s.png' % fruit,'wb')
-    f.write(urllib.urlopen('http://%s/static/img/%s.png' % (vault_host, fruit)).read())
-    f.close()
   # Run the webserver
   app.run(host='0.0.0.0', port=8080, debug=True)
